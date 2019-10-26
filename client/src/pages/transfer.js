4e7ebd5d47df976c45912e16f06cb1f793c40f9e
Peter Li <peter.s.li93@gmail.com>
2019-10-26 14:12:38 -0400

    transfer page

--- client/src/pages/transfer.js
+++ client/src/pages/transfer.js
@@ -3,13 +3,59 @@ import { Column, Row } from 'simple-flexbox';
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
 
-export default class Sidebar extends React.Component {
-    render() {
-        return (
-            <Column flexGrow={1} horizontal="center">
-                <h3> Transfer page </h3>
-                <span> eventually stuff will find their way here </span>
-            </Column>
-        )
-    }
+export default class Transfer extends React.Component {
+  render() {
+    return (
+      <Row flexGrow={1} horizontal="center">
+        <TransferForm />
+      </Row>
+    )
+  }
+}
+
+
+
+class TransferForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 2
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render() {
+    return (
+      <form>
+        <label>
+          Send CookieCash To:
+          <input
+            name="Receiver"
+            type="select"
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Amount:
+          <input
+            name="Amount"
+            type="number"
+            value={0}
+            onChange={this.handleInputChange} />
+        </label>
+      </form>
+    );
+  }
 }
