15a2642e752b9ea1eae0033e7fd828139d3a8709
Anderson <34724755+andersonaddo@users.noreply.github.com>
2019-10-26 12:38:19 -0400

    Skeletal job screen done

--- client/src/pages/jobs.js
+++ client/src/pages/jobs.js
@@ -3,13 +3,72 @@ import { Column, Row } from 'simple-flexbox';
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
 
-export default class Sidebar extends React.Component {
+export default class Jobs extends React.Component {
+
+    jobs = [{
+        name: "this is something",
+        status: "Available",
+        desc: "Blah Blah Blah Blahv Blah Blah Blah Blahv Blah Blah Blah Blahv"
+    },
+    {
+        name: "Do your goddamn homework",
+        status: "Available",
+        desc: "Blah Blah Blah Blahv Blah Blah Blah Blahv Blah Blah Blah Blahv"
+    },
+    {
+        name: "Meh this is anohter work",
+        status: "Available",
+        desc: "Blah Blah Blah Blahv Blah Blah Blah Blahv Blah Blah Blah Blahv"
+    },
+    {
+        name: "this is hilarious",
+        status: "Available",
+        desc: "Blah Blah Blah Blahv Blah Blah Blah Blahv Blah Blah Blah Blahv"
+    }]
+
     render() {
         return (
-            <Column flexGrow={1} horizontal="center">
-                <h3> Jobs page </h3>
-                <span> eventually stuff will find their way here </span>
+            <Column
+                style={{ height: "100%", width: "100%" }}
+                horizontal="stretch"
+                vertical="start">
+                <h3 style={{ textAlign: "center" }}> Jobs page </h3>
+                
+                {this.jobs.map((j) => <JobsSlide job = {j}/>)}
+                
             </Column>
+        )
+    }
+}
+
+class JobsSlide extends React.Component {
+    render() {
+        return (
+            <Row
+                vertical="center"
+                style={{
+                    borderBottom: "5px",
+                    borderColor: "grey",
+                    margin: "10px",
+                    backgroundColor: "grey",
+                }}>
+                <img
+                    src={require('../shared-assets/DefualtJobImage.png')}
+                    style={{ width: "60px", height: "60px", padding: 20 }} />
+                <Column>
+                    <h3> {this.props.job.name} </h3>
+                    <Row style={{ marginBottom: 10 }}>
+                        <span> Status </span>
+                        <span style={{ backgroundColor: "green", marginLeft: 15 }}> {this.props.job.status} </span>
+                    </Row>
+                    <span> {this.props.job.desc} </span>
+                    <Row horizontal="center">
+                        <button style={{ padding: 15, margin: 10 }}>
+                            Complete
+                            </button>
+                    </Row>
+                </Column>
+            </Row>
         )
     }
 }
