ef3f89a38264d53cb582f828b533901905496ea8
Nicholas Springer <nicholas.m.springer@gmail.com>
2019-10-26 19:38:12 -0400

    added models

--- models.py
+++ models.py
@@ -1,6 +1,35 @@
 from google.cloud import ndb
 
-class Job(nbd.Model):
+class Job(ndb.Model):
+    name = ndb.StringProperty()
+    value = ndb.IntegerProperty()
+
+class CompletedJob(ndb.Model):
+    job = ndb.StructuredProperty(Job)
+    timestamp = ndb.IntegerProperty()
+
+class ShopItem(ndb.Model):
+    name = ndb.StringProperty()
+    value = ndb.IntegerProperty()
     
+class PurchasedItem(ndb.Model):
+    item = ndb.StructuredProperty(ShopItem)
+    timestamp = ndb.IntegerProperty()
+
+class Bond(ndb.Model):
+    bond_type = ndb.StringProperty()
+    redeemed = ndb.BooleanProperty()
+    redeem_timestamp = ndb.IntegerProperty()
+
+class Child(nbd.Model):
+    name = ndb.StringProperty()
+    cookies = ndb.IntegerProperty()
+    completed_jobs = ndb.StructuredProperty(CompletedJob, repeated=True)
+    purchased_items = ndb.StructuredProperty(PurchasedItems, repeated=True)
+    bonds = ndb.StructuredProperty(Bond, repeated=True)
 
 class Account(ndb.Model):
+    email = ndb.StringProperty()
+    password =  ndb.StringProperty()
+    available_jobs = ndb.StructuredProperty(Job, repeated=True)
+    shop_items = ndb.StructuredProperty(ShopItem, repeated=True)
