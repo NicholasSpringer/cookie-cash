45eec02ae826c20d2ddb87792db53feda561b15e
Peter Li <peter.s.li93@gmail.com>
2019-10-26 19:20:52 -0400

    history page + icons

--- client/src/pages/history.js
+++ client/src/pages/history.js
@@ -2,14 +2,80 @@ import React from 'react';
 import { Column, Row } from 'simple-flexbox';
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
+export default class History extends React.Component {
 
-export default class Sidebar extends React.Component {
+
+    transactions = [{
+        type: "Bond",
+        timestamp: "Oct 26 11:16",
+        desc: "Bond Purchased: Short Term"
+    },
+    {
+        type: "Purchase",
+        timestamp: "Oct 26 11:16",
+        desc: "Brownie: 10 cookies"
+    },
+    {
+        type: "Transfer",
+        timestamp: "Oct 30 11:16",
+        desc: "Transferred 5 cookies to Sophia"
+    },
+    {
+        type: "Purchase",
+        timestamp: "Oct 26 11:16",
+        desc: "Pizza: 8 cookies"
+    }]
+
     render() {
         return (
-            <Column flexGrow={1} horizontal="center">
-                <h3> History page </h3>
-                <span> eventually stuff will find their way here </span>
+            <Column
+                style={{ height: "100%", width: "100%" }}
+                horizontal="stretch"
+                vertical="start">
+                <h3 style={{ textAlign: "center" }}>  Past Transactions </h3>
+
+                {this.transactions.map((i) => <TransactionSlide transaction={i} />)}
+
             </Column>
+        )
+    }
+
+}
+
+
+class TransactionSlide extends React.Component {
+    render() {
+        var imageSrc;
+        if (this.props.transaction.type == "Bond") {
+            imageSrc = '../shared-assets/BondImage.svg'
+        } else if (this.props.transaction.type == "Purchase") {
+            imageSrc = '../shared-assets/PurchaseImage.svg'
+        } else if (this.props.transaction.type == "Transfer") {
+            imageSrc = '../shared-assets/TransferImage.svg'
+        }
+        return (
+            <Row
+                vertical="center"
+                style={{
+                    borderBottom: "5px",
+                    borderColor: "grey",
+                    margin: "10px",
+                    backgroundColor: "grey",
+                }}>
+                <img
+                    src={require(imageSrc)}
+                    style={{ width: "60px", height: "60px", padding: 20 }} />
+                <Column>
+                    <h3> {this.props.transaction.type} </h3>
+                    <Row style={{ marginBottom: 10 }}>
+                        <span> Date </span>
+                        <span style={{ backgroundColor: "gold", marginLeft: 15 }}>
+                            {this.props.transaction.timestamp}
+                        </span>
+                    </Row>
+                    <span> {this.props.transaction.desc} </span>
+                </Column>
+            </Row>
         )
     }
 }
